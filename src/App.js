92bd0dcd679d139f3913df233df40e45524e6e11
guandjoy <guandjoy@gmail.com>
2019-11-28 23:45:41 +0200

    Set up RUDL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import Card from "./Card";
 import DraggableMasonryLayout from "react-universal-dnd-layout";
 import "./App.css";
 
 function App() {
-  const cards = Array.from(Array(10)).map((_, index) => <Card key={index} />);
-  return <div className="App">{cards}</div>;
+  const cards = Array.from(Array(10)).map((_, index) => (
+    <Card key={index} order={index} number={index + 1} id={index} />
+  ));
+  return (
+    <div className="wrapper">
+      <DraggableMasonryLayout key="layout-for-pinned-notes">
+        {cards}
+      </DraggableMasonryLayout>
+    </div>
+  );
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Card(props) {
-  return <div className="card" />;
+  console.log("card props", props);
+  return <div className="card" id={props.id} {...props.draggableItem} />;
 }
 
 export default Card;

8ec140d126c4a8c6d4708f5a63d9001ae2ec0323
Guandjoy <guandjoy@gmail.com>
2019-12-04 04:54:09 +0200

    Create Header

---
+++ src/application/misc/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "./Logo";
+import Description from "./Description";
+import GitHubBtn from "./GitHubBtn";
+// State
+import { useStateValue } from "../../state/state";
+
+function Header(props) {
+  // State
+  const [{ layoutWidth, cardsMargin }] = useStateValue();
+  return (
+    <div
+      className="header"
+      style={{
+        "--layout-width": `${layoutWidth}px`,
+        "--cards-margin": `${cardsMargin}px`
+      }}
+    >
+      <Logo />
+      {/* <Description /> */}
+      <GitHubBtn />
+    </div>
+  );
+}
+
+export default Header;

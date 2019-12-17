22e776d1eefade7e3e968915bc6fac5c7b4d34de
guandjoy <guandjoy@gmail.com>
2019-12-17 06:45:48 +0200

    Refactor root component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,110 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import App from "./application/App";
+import { StateProvider } from "./state/state";
+import Layout from "./application/layout/Layout";
+import Header from "./application/misc/Header";
+import LayoutSwitcher from "./application/layout/LayoutSwitcher";
+import JobSearch from "./application/misc/JobSearch";
+import "./styles/App.css";
+import "./styles/layout-switcher.css";
+import "./styles/dialog-window.css";
+import "./styles/gradients.css";
+import "@material/react-material-icon/dist/material-icon.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App() {
+  const masterWidth = () => {
+    let width = Math.round(window.innerWidth - (window.innerWidth / 100) * 4);
+    return width > 1900 ? 1900 : width;
+  };
+
+  const margin = layout => {
+    if (masterWidth() <= 640) return 4;
+    else return 12;
+  };
+
+  const borderWidth = () => (masterWidth() <= 640 ? 2 : 4);
+
+  const dialogBorderWidth = () => (masterWidth() <= 640 ? 4 : 8);
+
+  const tilesCardsWidth = () => {
+    let width = masterWidth();
+    let numOfColumns = width > 640 ? 3 : 2;
+    return width / numOfColumns - 2;
+  };
+
+  const tilesLayoutParams = () => {
+    const cardsWidth = tilesCardsWidth();
+    const cardsHeightRange = [cardsWidth, cardsWidth];
+    return {
+      layout: "tiles",
+      cardsWidth,
+      cardsHeightRange,
+      cardsMargin: margin(),
+      cardsBorderWidth: borderWidth(),
+      dialogBorderWidth: dialogBorderWidth()
+    };
+  };
+
+  const listCardsWidth = () => {
+    let width = masterWidth() - 2;
+    return width > 1000 ? 1000 : width;
+  };
+
+  const listLayoutParams = () => {
+    const cardsWidth = listCardsWidth();
+    const cardsHeightRange = [cardsWidth / 3, cardsWidth / 3];
+    return {
+      layout: "list",
+      cardsWidth,
+      cardsHeightRange,
+      cardsMargin: margin(),
+      cardsBorderWidth: borderWidth(),
+      dialogBorderWidth: dialogBorderWidth()
+    };
+  };
+
+  const masonryCardsWidth = () => {
+    let width = masterWidth();
+    let numOfColumns = width > 640 ? 4 : 3;
+    return width / numOfColumns - 2;
+  };
+
+  const masonryLayoutParams = () => {
+    const cardsWidth = masonryCardsWidth();
+    const cardsHeightRange = [cardsWidth / 2.5, cardsWidth * 1.4];
+    return {
+      layout: "masonry",
+      cardsWidth,
+      cardsHeightRange,
+      cardsMargin: margin(),
+      cardsBorderWidth: borderWidth(),
+      dialogBorderWidth: dialogBorderWidth()
+    };
+  };
+
+  const initialState = masonryLayoutParams();
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "tiles":
+        return tilesLayoutParams();
+      case "list":
+        return listLayoutParams();
+      case "masonry":
+        return masonryLayoutParams();
+      default:
+        return initialState;
+    }
+  };
+  return (
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <JobSearch />
+      <Header />
+      <Layout />
+      <LayoutSwitcher />
+    </StateProvider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
